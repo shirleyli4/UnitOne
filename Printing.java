cb7b7fb5634cd40fba181df53c1fcb2bd746ec3c
apple <apple@appledeMacBook-Air.local>
2019-09-10 15:17:14 -0400

    Part1 of Printing

---
+++ Printing.java
@@ -0,0 +1,17 @@
+
+public class Printing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("14:47");
+		System.out.println("09/10/19");
+		System.out.println("It is a great ");
+		System.out.println("time to be a ");
+		System.out.println("computer programmer!");
+		System.out.print("It is a great ");
+		System.out.print("time to be a ");
+		System.out.print("computer programmer!");
+
+	}
+
+}
